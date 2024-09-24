b876bdd86ce11703dee3610d85b294a5aedfd29b
mujtabahassan001 <mujtabahassaniqbal001@gmail.com>
2024-09-25 01:07:34 +0500

    Update Review.cs

--- Models/Entities/Review.cs
+++ Models/Entities/Review.cs
@@ -4,19 +4,18 @@ namespace LibraryAPI.Models.Entities
 {
     public class Review
     {
-        public int Id { get; set; }
-        public int BookId { get; set; } // Foreign key to Book
+    public int Id { get; set; }
 
-        [Required]
-        public string ReviewerName { get; set; } = string.Empty;
+    public int BookId { get; set; } // Foreign key to Book
 
-        [Required]
-        public int Rating { get; set; }
+    [Required]
+    public string ReviewerName { get; set; } = string.Empty;
 
-        [Range(1,5)]
-        public string? Comment { get; set; }
-
-        // Navigation property
-        public Book Book { get; set; }
+    [Required]
+    [Range(1, 5)]
+    public int Rating { get; set; }
+        
+    // Comment can be null and has no validation
+    public string? Comment { get; set; } 
     }
 }
